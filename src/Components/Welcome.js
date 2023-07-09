2646e94626effd18816cf3c8715db77ae0f28211
Arsh Arora <arsharora@umass.edu>
2023-07-09 13:08:45 -0400

    updated welcome page

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Welcome = () => {
   const navigate = useNavigate();
   const [isGlowing, setIsGlowing] = useState(false);
+  const [showHint, setShowHint] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowHint(true);
+    }, 5000);
+
+    return () => clearTimeout(timeout);
+  }, []);
 
   const handleLumosClick = () => {
     setIsGlowing(true);
@@ -15,13 +24,12 @@ const Welcome = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-black">
       <div className="text-white">
-        <h1
-          className={`text-6xl font-bold cursor-pointer ${isGlowing ? 'glow' : ''}`}
-          onClick={handleLumosClick}
-        >
+        <h1 className={`text-6xl font-bold cursor-pointer ${isGlowing ? 'glow' : ''}`} onClick={handleLumosClick}>
           LUMOS
         </h1>
-        <h2 className='text-center'>Click Lumos to Begin...</h2>
+        <h2 className={`text-center transition-opacity duration-1000 delay-500 ${showHint ? 'opacity-100' : 'opacity-0'}`}>
+          Hint: Press Lumos to begin
+        </h2>
       </div>
     </div>
   );
